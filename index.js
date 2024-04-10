e2954d7864d3df0bccd3b8a4784a9e79bf1188e0
Starkc137 <2451244@students.wits.ac.za>
2024-04-10 13:40:55 +0200

    1 Go

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //create cars api using express
 const express = require('express');
 const app = express();
+const path = require('path');
 
 
 
 app.use(express.json());
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname)));
 
 const cars = require('./cars.json');
 
@@ -46,7 +50,6 @@ app.post('/cars', (req, res) => {
     res.json(newCar);
 });
 
-//start app at localhost:3001
-app.listen(3001, () => {
-    console.log('Server started at http://localhost:3001');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
